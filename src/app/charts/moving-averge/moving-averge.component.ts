44a56b072ce12794e36a08ce4571f94da2ce8864
manikandan <mani.mdu89@gmail.com>
2024-05-23 09:46:56 +0530

    buy sell indicator

--- src/app/charts/moving-averge/moving-averge.component.ts
+++ src/app/charts/moving-averge/moving-averge.component.ts
@@ -1,66 +1,111 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
-import { createChart } from 'lightweight-charts'; 
+import { createChart } from 'lightweight-charts';
 import { ChartDataService } from '../services/chartData.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moving-averge',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './moving-averge.component.html',
   styleUrl: './moving-averge.component.css'
 })
-export class MovingAvergeComponent implements OnInit{
-  @ViewChild('MovingChart', {static: true})
-  chart: any| ElementRef;
-  
-  name = 'Chart';
+export class MovingAvergeComponent implements OnInit {
+  @ViewChild('MovingChart', { static: true })
+  chart: any | ElementRef;
 
-  constructor( private chartData: ChartDataService ) {}
+  name = 'Chart';
+  private createdChart: any;
+  private barData: any;
+  private maSeries: any
+  public lineSeriesArray: any[] = [];
+
+  constructor(private chartData: ChartDataService) { }
   ngOnInit() {
     const chartElement = this.chart.nativeElement;
-    const createdChart = createChart(chartElement, { 
+    this.createdChart = createChart(chartElement, {
       width: 500,
       height: 400,
       timeScale: {
         visible: true,
         timeVisible: true,
         secondsVisible: true,
-    },
+      },
     });
 
-    
-    const barData = this.chartData.generateCandleData(500);
-    const maData = this.chartData.calculateMovingAverageSeriesData(barData, 20);
-    const maSeries = createdChart.addLineSeries({ color: '#2962FF', lineWidth: 1 });
-    maSeries.setData(maData);
 
-    const candlestickSeries = createdChart.addCandlestickSeries({
-        upColor: '#26a69a',
-        downColor: '#ef5350',
-        borderVisible: false,
-        wickUpColor: '#26a69a',
-        wickDownColor: '#ef5350',
+    this.barData = this.chartData.generateCandleData(500);
+
+    const candlestickSeries = this.createdChart.addCandlestickSeries({
+      upColor: '#26a69a',
+      downColor: '#ef5350',
+      borderVisible: false,
+      wickUpColor: '#26a69a',
+      wickDownColor: '#ef5350',
     });
-    candlestickSeries.setData(barData);
+    candlestickSeries.setData(this.barData);
 
-    // const candlestickSeries = createdChart.addCandlestickSeries();
-    // const data: any = [
-    //   { time: '2018-06-25T09:15:00.000Z', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-    //   { time: '2018-06-25T09:20:00.000Z', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-    //   { time: '2018-06-25T09:25:00.000Z', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-    //   { time: '2018-06-25T09:30:00.000Z', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-    //   { time: '2018-06-25T09:35:00.000Z', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-    //   { time: '2018-06-25T09:40:00.000Z', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-    //   { time: '2018-06-25T09:45:00.000Z', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-    //   { time: '2018-06-25T09:50:00.000Z', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-    //   { time: '2018-06-25T09:55:00.000Z', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-    //   { time: '2018-06-25T10:00:00.000Z', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-    // ].map((d) => {
-    //   return { ...d, time: new Date(d.time).valueOf() / 1000 };
-    // });
-    // candlestickSeries.setData(data);
+    const datesForMarkers = [this.barData[this.barData.length - 39], this.barData[this.barData.length - 19]];
+    let indexOfMinPrice = 0;
+    for (let i = 1; i < datesForMarkers.length; i++) {
+      if (datesForMarkers[i].high < datesForMarkers[indexOfMinPrice].high) {
+        indexOfMinPrice = i;
+      }
+    }
 
-    createdChart.timeScale().fitContent();
+    const markers: any = [
+      {
+        time: this.barData[this.barData.length - 48].time,
+        position: 'aboveBar',
+        color: '#f68410',
+        shape: 'circle',
+        text: 'D',
+      },
+    ];
+    for (let i = 0; i < datesForMarkers.length; i++) {
+      if (i !== indexOfMinPrice) {
+        markers.push({
+          time: datesForMarkers[i].time,
+          position: 'aboveBar',
+          color: '#e91e63',
+          shape: 'arrowDown',
+          text: 'Sell @ ' + Math.floor(datesForMarkers[i].high + 2),
+        });
+      } else {
+        markers.push({
+          time: datesForMarkers[i].time,
+          position: 'belowBar',
+          color: '#2196F3',
+          shape: 'arrowUp',
+          text: 'Buy @ ' + Math.floor(datesForMarkers[i].low - 2),
+        });
+      }
+    }
+    candlestickSeries.setMarkers(markers);
+    this.createdChart.timeScale().fitContent();
+  }
+
+  public addLineSeries() {
+    const maData = this.chartData.calculateMovingAverageSeriesData(this.barData, 20 * (this.lineSeriesArray.length + 1));
+    const lineSeries = this.createdChart.addLineSeries({ color: this.getRandomColor(), lineWidth: 5 });
+    lineSeries.setData(maData);
+    this.lineSeriesArray.push(lineSeries);
+  }
+
+  public removeLineSeries(index: number) {
+    if (index >= 0 && index < this.lineSeriesArray.length) {
+      this.createdChart.removeSeries(this.lineSeriesArray[index]);
+      this.lineSeriesArray.splice(index, 1); // Remove the line series from the array
+    }
+  }
+
+  private getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 }
